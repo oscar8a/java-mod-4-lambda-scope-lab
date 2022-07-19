7e119d840f078ff88374ec1310ac24970ac2850a
Oscar 8A <oscarochoacerrato@gmail.com>
2022-07-19 15:29:33 -0400

    make prefix & suffix static to be used in Unary operator lambda expression

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,14 @@
 import java.util.function.UnaryOperator;
 
 public class Main {
-    String PREFIX = "__prefix__";
-    String SUFFIX = "__suffix__";
+    static String PREFIX = "__prefix__";
+    static String SUFFIX = "__suffix__";
 	
 	// fix the operator code
-    public static UnaryOperator<String> operator = str -> str;
+    public static UnaryOperator<String> operator = str -> PREFIX + str.trim() + SUFFIX;
 
     public static void main(String[] args) {
-        String res = operator.apply("  hello"); 
+        String res = operator.apply("  hello");
         System.out.println(res); // __prefix__hello__suffix__
     }
 }
